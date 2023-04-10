a43004e39a35ac2d42976584b62eedb388cbc39b
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-04-10 09:53:58 -0300

    feat: update modal conditions
    Now users can click in anyplace of the modal to close it. If the userinput is empty it will trigger the modal. If's not empty it will be added to the list.

--- 04-starting-project-adding-user/src/App.js
+++ 04-starting-project-adding-user/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import UserForm from './components/UserForm'
 import UserList from './components/UserList'
+import Modal from './components/modal';
 
 const INITIAL_USERS = [
   {
@@ -23,19 +24,36 @@ const INITIAL_USERS = [
 const App = () => {
 
   const [users, setUsers] = useState(INITIAL_USERS)
+  const [openModal, setOpenModal] = useState(false)
 
   const addUserHandler = (enteredData) => {
+
+    if (enteredData.name === '') {
+      setOpenModal(true)
+      return
+    }
+
+    if (enteredData.age === "") {
+      setOpenModal(true)
+      return
+    }
+
     setUsers((prevUsers) => {
       return [enteredData, ...prevUsers]
     })
+
+
   }
 
-  console.log(users);
+  const closeModalHandler = () => {
+    setOpenModal(false)
+  }
 
   return (
     <div>
       <UserForm onAddUser={addUserHandler} />
       <UserList listed={users} />
+      <Modal isOpen={openModal} closeModal={closeModalHandler} />
     </div>
   );
 }
